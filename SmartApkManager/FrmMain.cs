02a2a2cb873bfa8890cf0f515374ce0bc22022ab
Smart8bits <smart8bits@gmail.com>
2015-06-20 23:07:52 +0430

    Now Can Add Apks From A Folder To DB and WorkSpace

--- SmartApkManager/FrmMain.cs
+++ SmartApkManager/FrmMain.cs
@@ -21,7 +21,7 @@ namespace SmartApkManager
         
         private void BtnAddApk_Click(object sender, EventArgs e)
         {
-            SqlConnection DbConnection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\smart8\Documents\GitHub\SmartApkManager\SmartApkManager\Database1.mdf;Integrated Security=True"); 
+            SqlConnection DbConnection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\smart8\Documents\GitHub\SmartApkManager\SmartApkManager\Database1.mdf;Integrated Security=true"); 
             APK apk = new APK(@"C:\Users\smart8\Desktop\app\com.android.chrome-2\base.apk");
             using (DbConnection)
             {
@@ -65,35 +65,51 @@ namespace SmartApkManager
                 FlwLyoExplorer.Controls.Clear();
                 foreach (string FileName in FilesInDir)
                 {
-                    ApkHolder apkHolder = new ApkHolder();
-                    apkHolder.APK = new APK(FileName);
-                    FlwLyoExplorer.Controls.Add(apkHolder);
+                    try
+                    {
+                        ApkHolder apkHolder = new ApkHolder();
+                        apkHolder.APK = new APK(FileName);
+                        FlwLyoExplorer.Controls.Add(apkHolder);
+                    }
+                    catch { }
                 }
             }
         }
 
         private void BtnAddToDb_Click(object sender, EventArgs e)
         {
+            SqlConnection DbConnection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\smart8\Documents\GitHub\SmartApkManager\SmartApkManager\Database1.mdf;Integrated Security=True");
+            DbConnection.Open();
             foreach (ApkHolder apkHolder in FlwLyoExplorer.Controls)
             {
-                SqlConnection DbConnection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\smart8\Documents\GitHub\SmartApkManager\SmartApkManager\Database1.mdf;Integrated Security=True");
-                APK apk = apkHolder.APK;
-                using (DbConnection)
+                try
                 {
-                    DbConnection.Open();
+                    APK apk = apkHolder.APK;
+                    string newFilePath = string.Format("{0}\\{1} {2}.apk", Properties.Settings.Default.WorkSpace, apk.details.applicationLabel, apk.details.versionName);
                     SqlCommand InsertApk = new SqlCommand("ProcInsertApk", DbConnection);
                     InsertApk.CommandType = CommandType.StoredProcedure;
                     InsertApk.Parameters.Add(new SqlParameter("@packageName", apk.details.packageName));
                     InsertApk.Parameters.Add(new SqlParameter("@ApplicationLabel", apk.details.applicationLabel));
                     InsertApk.Parameters.Add(new SqlParameter("@versionCode", apk.details.versionCode));
-                    InsertApk.Parameters.Add(new SqlParameter("@filePath", apk.details.filePath));
+                    InsertApk.Parameters.Add(new SqlParameter("@filePath", newFilePath));
                     InsertApk.Parameters.Add(new SqlParameter("@versionName", apk.details.versionName));
                     InsertApk.Parameters.Add(new SqlParameter("@icon", apk.details.icon));
                     SqlDataReader reader = InsertApk.ExecuteReader();
                     reader.Read();
-                    MessageBox.Show(reader[0].ToString());
+                    if ((int)reader[0] == 1)
+                    {
+                        File.Move(apk.details.filePath, newFilePath);
+                        apk.details.filePath = newFilePath;
+                    }
+                    //MessageBox.Show(reader[0].ToString());
+                    reader.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("err");
                 }
             }
+            DbConnection.Close();
         }
     }
 }
